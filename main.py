8b720e52e1a045e597043d9e246502d38ecb01b7
QuantRuu <sherlockruu@163.com>
2019-07-09 22:59:17 +0800

    绘制k线图雏形

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import plotKline
+import datetime
 
+stock_id = '600519.XSHG'
+end_date = datetime.date.today()
+interval = 30
+interval_day = datetime.timedelta(days=interval)
+start_date = end_date - interval_day
+plotKline.plotCandlesticks(stock_id,start_date,end_date,stock_id,start_date,end_date,interval)
--- plotKline.py
+++ plotKline.py
@@ -1,11 +1,9 @@
 from jqdatasdk import *
 import pyecharts.options as opts
-from pyecharts.charts import Kline
+from pyecharts.charts import Kline,Page,Line
 
 auth('13917211596','910722mu')
-# get stock trade info from jqdata
-data = get_bars('600519.XSHG', 100, unit='1d',fields=['date','open','high','low','close'],include_now=False,end_dt='2019-07-07')
-print(data)
+page = Page()
 
 # 定义k线图的提示框的显示函数
 def show_kline_data(params):
@@ -18,34 +16,43 @@ def show_kline_data(params):
     return "date = " + param.name + "<br/>" + "cci = " + param.value + "<br/>"
 
 
-# 画出K线图
-price = [[open, close, lowest, highest] for open, close, lowest, highest in
- zip(data['open'], data['close'], data['low'], data['high'])]
-kline = Kline()
-kline.add_xaxis(list(data['date']))
-kline.add_yaxis(
-  '日线',
-  price,
-  itemstyle_opts=opts.ItemStyleOpts(
-    color="#FF6347",
-    color0="#BCEE68",
-    border_color="#8A0000",
-    border_color0="#008F28",
+def getSingleCandlestick(stock_id, end_date, interval):
+  # get stock trade info from jqdata
+  stock_info = get_security_info(stock_id)
+  data = get_bars(stock_id, interval, unit='1d',fields=['date','open','high','low','close'],include_now=False,end_dt=end_date)
+  # print(data)
+  # 画出K线图
+  price = [[open, close, lowest, highest] for open, close, lowest, highest in
+  zip(data['open'], data['close'], data['low'], data['high'])]
+  kline = Kline()
+  kline.add_xaxis(list(data['date']))
+  kline.add_yaxis(
+    '日线',
+    price,
+    itemstyle_opts=opts.ItemStyleOpts(
+      color="#FF6347",
+      color0="#BCEE68",
+      border_color="#8A0000",
+      border_color0="#008F28",
+    )
   )
-)
-kline.set_global_opts(
-    xaxis_opts=opts.AxisOpts(is_scale=True),
-    yaxis_opts=opts.AxisOpts(
-        is_scale=True,
-        splitarea_opts=opts.SplitAreaOpts(
-            is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
-        ),
-    ),
-    datazoom_opts=[opts.DataZoomOpts(pos_bottom="-2%")],
-    title_opts=opts.TitleOpts(title="Kline"),
-)
+  kline.set_global_opts(
+      xaxis_opts=opts.AxisOpts(is_scale=True),
+      yaxis_opts=opts.AxisOpts(
+          is_scale=True,
+          splitarea_opts=opts.SplitAreaOpts(
+              is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
+          ),
+      ),
+      datazoom_opts=[opts.DataZoomOpts(pos_bottom="-2%")],
+      title_opts=opts.TitleOpts(title=stock_id + "-" + stock_info.display_name),
+  )
 
-kline.render()
+  page.add(kline)
 
 
+def plotCandlesticks(research_id, start_date, end_date, benchmark_id, b_start_date, b_end_date, interval):
+  getSingleCandlestick(research_id,end_date, interval)
+  getSingleCandlestick(benchmark_id,b_end_date, interval)
+  page.render()
 
